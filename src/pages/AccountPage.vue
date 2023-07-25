91980114fb50f48e62ddec8448c6d757e8927e56
Devryn <devryn.zub@gmail.com>
2023-07-25 00:43:38 -0700

    changes

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -8,22 +8,28 @@
           <input v-model="formData.name" id="name" type="text" required>
 
           <label for="picture">Profile Picture URL</label>
-          <input v-model="formData.picture" id="picture" type="text">
+          <input v-model="formData.picture" id="picture" type="text" required>
 
           <label for="bio">Bio</label>
           <textarea v-model="formData.bio" id="bio" rows="4"></textarea>
 
           <label for="coverImg">Cover Image URL</label>
-          <input v-model="formData.coverImg" id="coverImg" type="text">
+          <input v-model="formData.coverImg" id="coverImg" type="text" required>
 
           <label for="github">GitHub URL</label>
           <input v-model="formData.github" id="github" type="text">
+
+          <label for="github">Linkedin URL</label>
+          <input v-model="formData.linkedin" id="linkedin" type="text">
+
+          <label for="github">Resume</label>
+          <input v-model="formData.Resume" id="resume" type="text">
 
           <label for="email">Email</label>
           <input v-model="formData.email" id="email" type="email" required>
 
           <label for="graduated">Graduated</label>
-          <input v-model="formData.graduated" id="graduated" type="text" placeholder="False will Update Info">
+          <input v-model="formData.graduated" id="graduated" type="checkbox">
 
           <label for="class">Class</label>
           <input v-model="formData.class" id="class" type="text">
@@ -36,13 +42,37 @@
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { computed, popScopeId, reactive, ref } from 'vue';
 import { accountService } from '../services/AccountService.js';
 import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
 
 export default {
   setup() {
+
+    const isGraduated = ref(false);
+
+    const isChecked = computed({
+      get: () => isGraduated.value,
+      set: (newValue) => (isGraduated.value = newValue),
+    });
+
+    function convertToBoolean(value) {
+      return value === 'true';
+    }
+
+    async function updateAccountInfo() {
+      try {
+        const graduated = convertToBoolean(isChecked.value);
+
+        await updateAccount({ graduated });
+
+      } catch (error) {
+        Pop.error(error)
+      }
+    }
+
+
     const formData = reactive({
       name: '',
       picture: '',
@@ -53,6 +83,7 @@ export default {
       graduated: '',
       class: '',
     });
+
 
     async function updateAccount() {
       try {
@@ -66,6 +97,9 @@ export default {
     return {
       formData,
       updateAccount,
+      isGraduated,
+      isChecked,
+      updateAccountInfo,
     };
   },
 };
